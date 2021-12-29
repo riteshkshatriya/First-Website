143bf1a557b8cfa7f9579636d79727409ef3fb8e
Ritesh Kshatriya <riteshkshatriya@Riteshs-MacBook-Pro.local>
2021-12-30 01:30:25 +0530

    banner desing

--- src/component/banner.js
+++ src/component/banner.js
@@ -1,3 +1,5 @@
+import arrow from '../images/arrow.png';
+import banner from '../images/banner.gif';
 
 
 const Banner =()=>{
@@ -5,13 +7,24 @@ const Banner =()=>{
         
         <section className="main-banner-section text-white">
             <div className="container">
-                <div className="left-side aside-banner">
-                    <div className="banner-content">
-                        <span>THE TECH ZONE demo</span>
+                <div className="d-flex">
+                    <div className="left-side aside-banner">
+                        <div className="banner-content">
+                            <span className="banner-sub-heading">THE TECH ZONE</span>
+                            <h1 className="main-heading-banner">Best Website Designing & Development Team</h1>
+                            <a href="#" className="common-button btn mt-4">More Information  <img className="arrow-image" src = {arrow} alt="" /> </a>
+                            <div className="round"></div>
+                        </div>
+                        <div className="overlay-button">
+                            <a href="#" class="view-button-banner circle link-unstyled">View</a>
+                        </div>
+                    </div>
+                    <div className='right-side aside-banner'>
+                        <div className="main-banner-image">
+                            <img className="w-100" src = {banner} alt="" /> 
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="right-side aside-banner">
             </div>
         </section>
       
--- src/component/header.js
+++ src/component/header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+import arrow from '../images/arrow.png';
+
 // import "../../node_modules/react-slick/dist/";
 // import "slick-carousel/slick/slick-theme.css";
 import {
@@ -38,16 +40,23 @@ export default function Heading() {
         </Routes>
         <nav className="navbar navbar-expand-lg navbar-light  custom-navigation">
           <div className="container">
-            <a className="navbar-brand fw-bold text-white nav-logo" href="#">Logo</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav">
-                <a className="nav-link active" href="#">Home</a>
-                <a className="nav-link" href="#">Portfolio</a>
-                <a className="nav-link" href="#">Services</a>
-                <a className="nav-link" href="#">Contact</a>
+            <div className="d-flex w-100 align-items-center">
+              <div className="nav-left d-flex">
+                <a className="navbar-brand fw-bold text-white nav-logo" href="#">Logo</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div className="navbar-nav">
+                    <a className="nav-link active" href="#">Home</a>
+                    <a className="nav-link" href="#">Portfolio</a>
+                    <a className="nav-link" href="#">Services</a>
+                    <a className="nav-link" href="#">Contact</a>
+                  </div>
+                </div>
+              </div>
+              <div className="nav-right">
+                <a href="#" className="common-button button-color btn">Get In Touch <img className="arrow-image" src = {arrow} alt="" /></a>
               </div>
             </div>
           </div>
